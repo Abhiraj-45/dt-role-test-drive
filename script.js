0a136b9fd604035222b79a8d73d403c3b0b8951e
Abhiraj-45 <abhirajsinha52@gmail.com>
2025-12-31 11:15:08 +0530

    Update script.js
    JSON Committed

---
+++ script.js
@@ -0,0 +1,45 @@
+const sheetData = [
+  {
+    section: "Position",
+    content: "Frontend Intern",
+    side: {
+      title: "Task 1: Webpage Creation",
+      description:
+        "You are given a Figma file with the page design. You are asked to create HTML and CSS accordingly."
+    }
+  },
+  {
+    section: "Required Skillset",
+    content: "HTML, CSS, JavaScript",
+    side: {}
+  },
+  {
+    section: "DT Tech Stack",
+    content:
+      "MongoDB, Node.js, Express Framework, Confluence, Kafka, Nginx, React Native, Vanilla JavaScript",
+    side: {
+      title: "Task 2: Dynamic Elements",
+      description:
+        "Given JSON objects and functionality details. Execute a dynamic page with the functionality."
+    }
+  },
+  {
+    section: "About DeepTech",
+    content:
+      "A technology is considered DeepTech if a considerable amount of research has gone into its development. DeepThought has researched psychology to develop a new way of building EdTech applications.",
+    side: {
+      title: "Why these tasks",
+      description:
+        "These tasks assess comprehension of documentation, aesthetic sense, and execution of logic."
+    }
+  },
+  {
+    section: "DeepTech Mindset",
+    content:
+      "Ability to comprehend new ideas, think abstractly, brainstorm more, ask questions, and explore new possibilities through technology.",
+    side: {
+      description:
+        "All DT interns are trained on the complete tech stack, socio-psychological learning models, leadership, and DeepTech innovation."
+    }
+  }
+];
